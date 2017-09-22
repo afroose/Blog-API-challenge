b4f140acfd3daf5f45cf601cd98dae37d1b6bf01
afroose <arnaud.roose@gmail.com>
2017-09-22 12:30:22 -0400

    Adds put endpoint - tested

--- blogRouter.js
+++ blogRouter.js
@@ -28,4 +28,38 @@ router.post('/', jsonParser, (req,res) => {
   res.status(201).json(blog);
 })
 
+router.put('/:id', jsonParser, (req,res) => {
+  const requiredFields = ['id', 'title', 'content', 'author'];
+  // check valid fields
+  for (let i=0; i< requiredFields.length; i++) {
+    const field = requiredFields[i]
+    if(!(field in req.body)) {
+      const message = `Missing field: ${field} in request}`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  // check params id matches body id
+  if (req.params.id !== req.body.id) {
+    const message = `Request id mismatch: params=${req.params.id} - body=${req.body.id}`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  // update
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  })
+  const message=`Success`;
+  console.log(message);
+  res.status(204).send(message).end();
+})
+
+router.delete('/:id', (req,res) => {
+  BlogPosts.delete(req.params.id);
+  res.status(204).end();
+})
+
 module.exports = router;
